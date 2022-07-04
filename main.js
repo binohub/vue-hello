59bcd5384bfad6698dcdb930c8be9a249038c102
binohub <yeebino001@gmail.com>
2022-07-04 17:25:04 +0200

    vue - ms1 - done

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue.js!'
-    }
-  })
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            header: 'Dynamic Message',
+            imgSource: "https://images.pexels.com/photos/302743/pexels-photo-302743.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        }
+    });
