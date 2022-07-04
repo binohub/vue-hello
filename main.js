5824f64b1b0821d3da3f70cf4178b5d58c3724a8
binohub <yeebino001@gmail.com>
2022-07-04 17:31:54 +0200

    vue - bonus - finish

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = new Vue(
     {
         el: '#app',
         data: {
-            header: 'Dynamic Message',
-            imgSource: "https://images.pexels.com/photos/302743/pexels-photo-302743.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            header: '',
+            imgSource: "",
         }
     });
